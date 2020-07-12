94a9610dc3f1d587882862d38eaf50bf7ba4eb59
Jenae Christensen <729jenae.christensen@gmail.com>
2020-07-12 20:35:56 +0000

    Automatically backed up by Learn

--- db/migrate/20150915202821_add_column_to_cats.rb
+++ db/migrate/20150915202821_add_column_to_cats.rb
@@ -3,7 +3,7 @@ class AddColumnToCats < ActiveRecord::Migration
     add_column :cats, :owner_id, :integer
   end
 
-  def down
-    remove_column :cats, :owner_id
-  end
+  # def down
+  #   remove_column :cats, :owner_id
+  # end
 end
